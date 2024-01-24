b8e8589d80ade54cf95edee2f7e73511bff69bd0
Joshtri <stuffofyos1516@gmail.com>
2024-01-24 09:34:42 +0800

    lts

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,7 +3,7 @@ mongoose.set('strictQuery', false);
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI, {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
             // Tambahkan opsi timeout di sini (misalnya, 30 detik)
             serverSelectionTimeoutMS: 100000,
         });
