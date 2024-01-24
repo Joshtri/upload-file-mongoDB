76a0d4b8a15d81cbef23897d799e82c1e2420b62
Joshtri <stuffofyos1516@gmail.com>
2024-01-24 09:25:10 +0800

    lts

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -4,16 +4,14 @@ mongoose.set('strictQuery', false);
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI, {
-            // Set timeout ke 30 detik
-            serverSelectionTimeoutMS: 30000,
+            // Tambahkan opsi timeout di sini (misalnya, 30 detik)
+            serverSelectionTimeoutMS: 100000,
         });
 
         console.log(`Database connected. ${conn.connection.host}`);
     } catch (error) {
-        console.error("Error connecting to the database:", error);
-        res.status(500).send("Internal Server Error");
+        console.error(error);
     }
-    
 };
 
 module.exports = connectDB;
